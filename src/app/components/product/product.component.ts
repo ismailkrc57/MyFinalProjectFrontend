2d11a6f6b1167c5e1407cf8ace654f686d221639
ismailkrc57 <ismailkrc57@gmail.com>
2021-12-16 14:28:29 +0300

    spinner added for products table

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,18 +9,19 @@ import { Product } from 'src/app/models/product';
 })
 export class ProductComponent implements OnInit {
   products: Product[] = [];
+  dataLoaded: boolean = false;
 
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.getProducts();
     // test
-
   }
 
   getProducts() {
     this.productService.getProducts().subscribe((response) => {
       this.products = response.data;
+      this.dataLoaded = true;
     });
   }
 }
